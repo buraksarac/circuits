bff22f2266e99938fba236528ba67683a8f7c432
burak <burak@linux.com>
2019-11-20 11:02:33 +0100

    example test

---
+++ src/test/java/org/qunix/circuits/leetcode/ValidateDouble.java
@@ -0,0 +1,69 @@
+package org.qunix.circuits.leetcode;
+
+import org.junit.Test;
+import org.qunix.circuits.CircuitCondition;
+import org.qunix.circuits.CircuitCondition.ConditionMismatchException;
+import org.qunix.circuits.Circuits;
+
+public class ValidateDouble {
+
+	@Test
+	public void testDouble() {
+		// test 12.054e1
+		CircuitCondition<Character> digit = Circuits.between('0', '9');
+		CircuitCondition<Character> decimal = Circuits.singlePass('.');
+		CircuitCondition<Character> exponent = Circuits.singlePass('e');
+
+		digit.ignore(decimal, exponent);
+		digit.when(decimal).expect().circuitOpen();
+		decimal.when(exponent).expect().circuitOpen();
+
+		Circuits<Character> circuits = Circuits.of(digit, decimal, exponent);
+
+		char[] chars = "12.0e541".toCharArray();
+		for (char c : chars) {
+			circuits.accept(c);
+		}
+
+	}
+
+	@Test(expected = ConditionMismatchException.class)
+	public void testDouble2() {
+		// test 12.054.1
+		CircuitCondition<Character> digit = Circuits.between('0', '9');
+		CircuitCondition<Character> decimal = Circuits.singlePass('.');
+		CircuitCondition<Character> exponent = Circuits.singlePass('e');
+
+		digit.ignore(decimal, exponent);
+		digit.when(decimal).expect().circuitOpen();
+		decimal.when(exponent).expect().circuitOpen();
+
+		Circuits<Character> circuits = Circuits.of(digit, decimal, exponent);
+
+		char[] chars = "12.054.1 ".toCharArray();
+		for (char c : chars) {
+			circuits.accept(c);
+		}
+
+	}
+
+	@Test(expected = ConditionMismatchException.class)
+	public void testDouble3() {
+		// test 12e.054e1
+		CircuitCondition<Character> digit = Circuits.between('0', '9');
+		CircuitCondition<Character> decimal = Circuits.singlePass('.');
+		CircuitCondition<Character> exponent = Circuits.singlePass('e');
+
+		digit.ignore(decimal, exponent);
+		digit.when(decimal).expect().circuitOpen();
+		decimal.when(exponent).expect().circuitOpen();
+
+		Circuits<Character> circuits = Circuits.of(digit, decimal, exponent);
+
+		char[] chars = "12e.054e1".toCharArray();
+		for (char c : chars) {
+			circuits.accept(c);
+		}
+
+	}
+}
