692e70d959886c0d85f1b11e6e5fc3b60fcd8af1
burak <burak@linux.com>
2019-11-22 10:22:19 +0100

    test fix

--- src/test/java/org/qunix/circuits/leetcode/RemoveInvalidParentheses.java
+++ src/test/java/org/qunix/circuits/leetcode/RemoveInvalidParentheses.java
@@ -1,8 +1,8 @@
 package org.qunix.circuits.leetcode;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.LinkedList;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -20,12 +20,13 @@ public class RemoveInvalidParentheses {
 
 	Circuit<Character> circuit;
 	StringBuilder sb = new StringBuilder();
-	AtomicInteger lastOpenIndex = new AtomicInteger();
+	LinkedList<Integer> lastOpenIndexes = new LinkedList<>();
 
 	@Before
 	public void setup() {
 		circuit = Circuits.biCircuit('(', ')').nested();
-		circuit.onOpen(c -> lastOpenIndex.set(sb.length()));
+		circuit.onOpen(c -> lastOpenIndexes.offer(sb.length()));
+		circuit.onClose(c -> lastOpenIndexes.poll());
 	}
 
 	@Test
@@ -48,15 +49,52 @@ public class RemoveInvalidParentheses {
 		test(")(test", "test");
 	}
 
+	@Test
+	public void test5() {
+		test("(a)(()))()", "(a)(())()");
+	}
+
+	@Test
+	public void test6() {
+		test("))sad0asd0a)A)SD())D)()((D)AS))S))))", "sad0asd0aASD()D()((D)AS)S");
+	}
+
+	@Test
+	public void test7() {
+		test("", "");
+	}
+
+	@Test
+	public void test8() {
+		test("[", "[");
+	}
+
+	@Test
+	public void test9() {
+		test("(((((((((", "");
+	}
+
+	@Test
+	public void test10() {
+		test("(((a((b(((c(", "abc");
+	}
+
+	@Test
+	public void test11() {
+		test(")))))", "");
+	}
+
 	private void test(String source, String expect) {
 		for (char c : source.toCharArray()) {
 			circuit.ifAccept(c, sb::append);
 		}
 
-		if (circuit.isOpen()) { 
-			sb.deleteCharAt(lastOpenIndex.get());
+		if (circuit.isOpen()) {
+			while (!lastOpenIndexes.isEmpty()) {
+				sb.deleteCharAt(Math.max(0, lastOpenIndexes.pollLast()));
+			}
 		}
 
-		assertTrue(expect.equals(sb.toString()));
+		assertEquals(expect, sb.toString());
 	}
 }
